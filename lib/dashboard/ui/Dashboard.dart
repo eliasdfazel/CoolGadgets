cca529b6d2010bf47b9e5e3da72349ac6a1a66c0
Elias Fazel <elias.fazel@gmail.com>
2024-07-06 18:21:37 -0400

    Initialize Cool Gadgets for Project

--- lib/dashboard/ui/Dashboard.dart
+++ lib/dashboard/ui/Dashboard.dart
@@ -10,7 +10,24 @@ class Dashboard extends StatefulWidget {
   @override
   State<Dashboard> createState() => DashboardState();
 }
-class DashboardState extends State<Dashboard> {
+class DashboardState extends State<Dashboard> with TickerProviderStateMixin {
+
+  /*
+   * Start - Menu
+   */
+  late AnimationController animationController;
+
+  late Animation<Offset> offsetAnimation;
+  late Animation<double> scaleAnimation;
+  BorderRadius radiusAnimation = BorderRadius.circular(0);
+
+  late Animation<Offset> offsetAnimationItems;
+  double opacityAnimation = 0.37;
+
+  bool menuOpen = false;
+  /*
+   * End - Menu
+   */
 
   bool aInterceptor(bool stopDefaultButtonEvent, RouteInfo info) {
 
@@ -25,6 +42,28 @@ class DashboardState extends State<Dashboard> {
 
     BackButtonInterceptor.add(aInterceptor);
 
+    animationController = AnimationController(vsync: this,
+        duration: const Duration(milliseconds: 777),
+        reverseDuration: const Duration(milliseconds: 333),
+        animationBehavior: AnimationBehavior.preserve);
+
+    offsetAnimation = Tween<Offset>(begin: const Offset(0, 0), end: const Offset(0.51, 0))
+        .animate(CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeIn
+    ));
+    scaleAnimation = Tween<double>(begin: 1, end: 0.91)
+        .animate(CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeOut
+    ));
+
+    offsetAnimationItems = Tween<Offset>(begin: const Offset(-0.19, 0), end: const Offset(0, 0))
+        .animate(CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeIn
+    ));
+
   }
 
   @override
@@ -38,19 +77,35 @@ class DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
 
-    return SafeArea(
-      child: Scaffold(
-          resizeToAvoidBottomInset: true,
-          backgroundColor: ColorsResources.premiumDark,
-          body: Stack(
-              children: [
+    return Scaffold(
+        resizeToAvoidBottomInset: true,
+        backgroundColor: ColorsResources.premiumDark,
+        body: Stack(
+            children: [
+
+              /*
+               * Start - Menu
+               */
+
+              /*
+               * End - Menu
+               */
 
 
 
-              ]
-          )
-      )
+              /*
+               * Start - Content
+               */
+
+              /*
+               * End - Content
+               */
+
+            ]
+        )
     );
   }
+
+
 
 }
