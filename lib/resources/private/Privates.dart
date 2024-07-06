b729c2f91815a7c5fc674ccfd67cb89695875f76
Elias Fazel <elias.fazel@gmail.com>
2024-07-06 14:46:49 -0400

    Initialize Cool Gadgets for Project

--- lib/resources/private/Privates.dart
+++
@@ -1,7 +0,0 @@
-class Privates {
-  static const String authenticationAPI = 'Basic Z2Vla3NlbXBpcmVpbmM6KmdYZW1waXJlIzEwMjk2JA==';
-
-  static const String reCaptchEnterpriseSiteKey = '';
-
-  static const String aiKeyAPI = '';
-}
