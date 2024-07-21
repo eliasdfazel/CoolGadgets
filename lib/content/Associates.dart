db29a07a0fbb03b36a8c631c1ad3e78c50757668
Elias Fazel <elias.fazel@gmail.com>
2024-07-21 14:40:11 -0400

    Cool Gadgets for...

--- lib/content/Associates.dart
+++ lib/content/Associates.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 
 Widget associatesBranding() {
 
-  return SizedBox(
-    height: 31,
-    width: 177,
-    child: Image.asset(
-      'images/associate_branding.png'
+  return Container(
+    padding: const EdgeInsets.only(top: 37, bottom: 37),
+    alignment: Alignment.center,
+    child: SizedBox(
+        height: 31,
+        width: 177,
+        child: Image.asset(
+            'images/associate_branding.png'
+        )
     )
   );
 }
